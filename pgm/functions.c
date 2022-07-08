94e8a6bdbb06d8e0c27b56a8d2a24bff017954fd
Yohans Nascimento <yohans.dev@gmail.com>
2022-07-08 10:15:15 -0300

    Criando Funcao escreverArquivo

--- pgm/functions.c
+++ pgm/functions.c
@@ -3,8 +3,7 @@
 #include <time.h>
 
 
-int histogramao(int vetorDec[], int c, int r)
-{
+int histogramaVetor(int vetorDec[], int c, int r){
     int total =  c * r;
     int histograma[total];
 
@@ -24,4 +23,15 @@ int histogramao(int vetorDec[], int c, int r)
         }
     }
     return histograma;
+}
+
+int escreverArquivo(){
+    FILE *file;
+    file = fopen("SaidaFinal.txt","w");//Ver como os dados devem ser gravados pq se for abrir o arquivo para cada linha devemos trocar o "w" por "a" para que ele nao sobreescreva e sim adicione.
+    if(file == NULL){
+        printf("Ocorreu um erro na aberura do arquivo.");
+        exit(1);
+    }
+    fprintf(file, " \n"); //Aqui, entre "" sera passado as linhas que devem ser salvas no arquivo.
+    fclose(file);
 }
